aed30e4a74f5bff88773146f0d8f7ff842f9a040
Augusto Dwenger J <dwenger@posteo.de>
2021-03-08 16:33:14 +0100

    Remove cobra as cli flag manager
    This reduces the package size by ca. 50%. One draw back of this
    change is that there is no longer support for the `config.yml` file.
    There is also a change inside the env vars naming: The variables have to
    start with `RCON_CLI`.

--- go.mod
+++ go.mod
@@ -1,11 +1,8 @@
 module github.com/itzg/rcon-cli
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/james4k/rcon v0.0.0-20120923215419-8fbb8268b60a
-	github.com/spf13/cobra v0.0.3
-	github.com/spf13/viper v1.3.1
+	github.com/james4k/rcon v0.0.0-20210222224819-34a67ca2b2d6
+	github.com/spf13/pflag v1.0.5
 )
 
 go 1.13
--- main.go
+++ main.go
@@ -1,7 +1,102 @@
 package main
 
-import "github.com/itzg/rcon-cli/cmd"
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/itzg/rcon-cli/cli"
+	flags "github.com/spf13/pflag"
+)
+
+const (
+	defaultHost     = "localhost"
+	defaultPort     = "25575"
+	defaultPassword = ""
+
+	envVarPrefix = "RCON_CLI_"
+)
+
+// vars holding the parsed configuration
+var (
+	host     string
+	port     string
+	password string
+	help     bool
+)
 
 func main() {
-	cmd.Execute()
+	commands, _ := parseArgs(os.Args[1:])
+	loadEnvIfNotSet()
+	if help {
+		printHelp()
+		os.Exit(0)
+		return
+	}
+
+	run(commands...)
+}
+
+func parseArgs(args []string) ([]string, error) {
+	flags.StringVar(&host, "host", defaultHost, "RCON server's hostname.")
+	flags.StringVar(&port, "port", defaultPort, "RCON server's port.")
+	flags.StringVar(&password, "password", defaultPassword, "RCON server's password.")
+	flags.BoolVarP(&help, "help", "h", false, "Prints this help message and exits.")
+
+	err := flags.CommandLine.Parse(args)
+	if err != nil {
+		return []string{}, err
+	}
+
+	command := flags.Args()
+
+	return command, nil
+}
+
+func loadEnvIfNotSet() {
+	envHost := os.Getenv(fmt.Sprintf("%sHOST", envVarPrefix))
+	if envHost != "" && host == defaultHost {
+		host = envHost
+	}
+
+	envPort := os.Getenv(fmt.Sprintf("%sPORT", envVarPrefix))
+	if envPort != "" && port == defaultPort {
+		port = envPort
+	}
+
+	envPassword := os.Getenv(fmt.Sprintf("%sPASSWORD", envVarPrefix))
+	if envPassword != "" && password == defaultPassword {
+		password = envPassword
+	}
+}
+
+func run(args ...string) {
+	hostPort := net.JoinHostPort(host, port)
+
+	if len(args) == 0 {
+		cli.Start(hostPort, password, os.Stdin, os.Stdout)
+	} else {
+		cli.Execute(hostPort, password, os.Stdout, args...)
+	}
+}
+
+func printHelp() {
+	fmt.Println(`rcon-cli is a CLI to interact with a RCON server.
+It can be run in an interactive mode or to execute a single command.
+
+USAGE:
+	rcon-cli [FLAGS] [RCON command ...]
+	
+FLAGS:`)
+	flags.PrintDefaults()
+	fmt.Printf(`
+ENVIRONMENT VARIABLE:
+	All flags can be set through the flag name in capslock with the %s prefix (see examples).
+	Flags have allways priority over env vars!
+
+EXAMPLES:
+	rcon-cli --host 127.0.0.1 --port 25575
+	rcon-cli --password admin123 stop
+	%sPORT=25575 rcon-cli stop
+`, envVarPrefix, envVarPrefix)
 }
